f3c989fe5f428b7886e06c8ea15b8f301dd43321
fabrizionkele1b <fabrizionkele1b@gmail.com>
2025-12-06 10:55:46 +0530

    Clean repo and update .gitignore

--- dist/src/api/seo-setting/controllers/seo-setting.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * seo-setting controller
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-const strapi_1 = require("@strapi/strapi");
-exports.default = strapi_1.factories.createCoreController('api::seo-setting.seo-setting');
--- dist/src/api/site-config/services/site-config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * site-config service
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-const strapi_1 = require("@strapi/strapi");
-exports.default = strapi_1.factories.createCoreService('api::site-config.site-config');
